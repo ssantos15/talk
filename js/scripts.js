532ed9a638d15d2ca523f853cdd3c8798ced2434
Steve Santos <padma@email.com>
2018-09-20 14:01:48 -0700

    Just added a remove method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,33 @@
 $(function() {
   $("#hello").click(function() {
     $("ul#user").append("<li>Hello</li>");
-    $("ul#webpage").append("<li>Hi there!!</li>");
+    $("ul#webpage").append("<li>Hello Muggle!!</li>");
     $("ul#user").children("li").last().click(function() {
-      alert("hi computer");
+      $(this).remove();
     });
     $("ul#webpage").children("li").last().click(function() {
-      alert("Hello Muggle");
+      $(this).remove();
     });
   });
   $("#goodbye").click(function() {
     $("ul#user").append("<li>Goodbye</li>");
     $("ul#webpage").append("<li>See ya later you muggle!!</li>");
+    $("ul#user").children("li").last().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").last().click(function() {
+      $(this).remove();
+    });
   });
   $("#stop").click(function() {
     $("ul#user").append("<li>STOP COPYING ME!!!!</li>");
     $("ul#webpage").append("<li>Expeleomus!!!!!</li>");
+    $("ul#user").children("li").last().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").last().click(function() {
+      $(this).remove();
+    });
   });
   $("#bgColor").click(function() {
     $("body").toggleClass("backgroundColor");
